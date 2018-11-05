afc60a7a98625a4e29d61aec33793f7a8dfdb80e
monnier.charly@orange.fr <32869761+charly33@users.noreply.github.com>
2018-11-05 18:00:47 +0100

    Add shoot target

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.score = 0
 
 shooter.addTarget = () => 
 {
@@ -13,6 +14,22 @@ shooter.addTarget = () =>
     $target.style.top = `${Math.random()*100}%`
     $target.style.left = `${Math.random()*100}%`
     shooter.$targets.appendChild($target)
+    
+    $target.addEventListener('mouseenter', () =>
+    {
+        shooter.shootTarget($target)
+    })
+}
+shooter.shootTarget = (_$target) =>
+{
+    // Delete target
+    _$target.remove()
+    // Add new target
+    shooter.addTarget()
+    // Increment score
+    shooter.score++
+    shooter.$score.textContent = shooter.score
+    // Play sound
 }
 
 shooter.addTarget()
